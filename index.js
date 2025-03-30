2e13da5a5e6869c1b1fac52d5a40746eec98c34f
tojiboyev717 <Tojiboyevumid717@gmail.com>
2025-03-30 12:28:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ require('./keep_alive'); // Keep-alive serverni ishga tushirish
 const mineflayer = require('mineflayer');
 require('colors').enable();
 
-// Qolgan bot kodi shu yerda davom etadi...
-
-const botUsername = 'FN_06';
+const botUsername = 'FN_01';
 const botPassword = 'fortune321';
 const admin = 'Umid';
+var playerList = [];
 var mcData;
 
 const botOption = {
@@ -46,108 +45,138 @@ function init() {
         if (message.startsWith("Skyblock »")) return;
         console.log(message);
 
+        // Server restart bo'lsa chiqish
         if (message === "Server: Serverni kunlik restartiga 30 sekund qoldi") {
             bot.quit("20min");
         }
 
+        // Ro‘yxatdan o‘tish yoki login qilish
         if (message.includes("register")) {
             bot.chat(`/register ${botPassword} ${botPassword}`);
         }
         if (message.includes("login")) {
             bot.chat(`/login ${botPassword}`);
         }
-        if (message.includes("Вы успешно вошли в аккаунт")) {
+		if (message.includes("Вы успешно вошли в аккаунт")) {
             bot.chat(`/is warp sell`);
+        }
+
+        // Hisobdagi pullarni avtomatik yuborish
+        if (message.includes("Balance: $")) {
+            let balanceStr = message.match(/Balance: \$([\d,]+)/);
+            if (!balanceStr || balanceStr.length < 2) return;
+
+            let balance = parseInt(balanceStr[1].replace(/,/g, ""));
+
+            if (balance > 0) {
+                bot.chat(`/pay ${admin} ${balance}`);
+            }
         }
     });
 
+    // Admindan buyruqlarni bajarish
     bot.on("whisper", (usernameSender, message) => {
-        if (usernameSender === admin && message.startsWith("6 ")) {
-            const command = message.replace("6 ", "");
+        if (usernameSender === admin && message.startsWith("!1 ")) {
+            const command = message.replace("!1 ", "");
             bot.chat(command);
         }
     });
 
-    bot.on('windowOpen', async (window) => {
+
+    // Chestdan honey olish va sotish
+bot.on('windowOpen', async (window) => {
         setTimeout(() => {
             bot.closeWindow(window);
         }, 19000);
-
         if (window.title.includes('Island Shop | Food')) {
-            let honeyCount = bot.inventory.slots.reduce((sum, slot) => {
-                return slot?.name === 'honey_bottle' ? sum + slot.count : sum;
-            }, 0);
-
+            let honeyCount = 0;
+            bot.inventory.slots.forEach(slot => {
+                if (slot?.type != undefined && slot?.type != null && slot?.name == 'honey_bottle') {
+                    honeyCount += slot?.count;
+                }
+            });
             for (let i = 0; i < honeyCount; i++) {
                 setTimeout(() => {
                     bot.simpleClick.rightMouse(21, 0, 0);
                 }, 20);
             }
-
             setTimeout(async () => {
                 await bot.closeWindow(window);
                 bot.chat('/is warp sell');
-                bot.chat('/is withdraw money 9999999999999')
+                bot.chat('/is withdraw money 9999999999999999');
                 bot.chat('/bal');
             }, honeyCount * 20 + 100);
-        }
-    });
-}
-
-async function withdrawHoney(bot, mcData) {
-    bot.chat('/is warp sell');
-    setTimeout(async () => {
-        var chestPosition = await bot.findBlock({
-            matching: mcData.blocksByName.chest.id,
-            maxDistance: 5,
-        });
-        if (!chestPosition) return;
-
-        let attempts = 0;
-        let chest = null;
-        const maxAttempts = 3;
-
-        while (!chest && attempts < maxAttempts) {
-            try {
-                chest = await bot.openChest(chestPosition);
-            } catch (error) {
-                console.log(`Error opening chest: ${error}. Retrying...`);
-                attempts++;
-                if (error.includes(`timeout of 20000ms`)) {
-                    await bot.quit('reconnect');
-                }
-            }
-        }
-
-        if (!chest) {
-            console.log("Failed to open chest after multiple attempts.");
             return;
         }
+    });
+	
+    async function withdrawHoney(bot, mcData) {
+        bot.chat('/is warp sell');
+        setTimeout(async () => {
+            var chestPosition = await bot.findBlock({
+                matching: mcData.blocksByName.chest.id,
+                maxDistance: 5,
+            });
+            if (!chestPosition) return;
 
-        function hasFreeSlot() {
-            return bot.inventory.emptySlotCount() > 0;
-        }
+            // Implement retry logic
+            let attempts = 0;
+            let chest = null;
+            const maxAttempts = 3;
 
-        for (let slot of chest.slots) {
-            if (slot?.name === 'honey_bottle' && slot?.count > 0) {
-                while (slot.count > 0 && hasFreeSlot()) {
-                    let countToWithdraw = Math.min(slot.count, bot.inventory.itemLimit - slot.count);
-                    try {
-                        await chest.withdraw(slot.type, null, countToWithdraw);
-                        slot.count -= countToWithdraw;
-                    } catch (error) {
+            while (!chest && attempts < maxAttempts) {
+                try {
+                    chest = await bot.openChest(chestPosition);
+                } catch (error) {
+                    console.log(`Error opening chest: ${error}. Retrying...`);
+                    attempts++;
+                    console.log(error);
+                    if (error.includes(`timeout of 20000ms`)) {
+                        await bot.quit('reconnect')
+                    }
+                    // await new Promise(resolve => setTimeout(resolve, 5000));
+                }
+            }
+
+            if (!chest) {
+                console.log("Failed to open chest after multiple attempts.");
+                return;
+            }
+
+            // Function to check if there are any free slots in the inventory
+            function hasFreeSlot() {
+                return bot.inventory.emptySlotCount() > 0;
+            }
+
+            // Function to check if there are any honey bottles left in the chest
+            function honeyLeftInChest(chest) {
+                return chest.slots.some(slot => slot?.type !== undefined && slot?.type !== null && slot?.name === 'honey_bottle' && slot?.count > 0);
+            }
+
+            // Iterate through the chest slots and withdraw honey bottles
+            for (let slot of chest.slots) {
+                if (slot?.type !== undefined && slot?.type !== null && slot?.name === 'honey_bottle' && slot?.count > 0) {
+                    while (slot.count > 0 && hasFreeSlot()) {
+                        let countToWithdraw = Math.min(slot.count, bot.inventory.itemLimit - slot.count);
+                        try {
+                            await chest.withdraw(slot.type, null, countToWithdraw);
+                            slot.count -= countToWithdraw;
+                        } catch (error) {
+                            // console.log(`Error withdrawing items: ${error}`);
+                            break;
+                        }
+                    }
+                    if (!hasFreeSlot()) {
+                        // console.log("Inventory is full, stopping withdrawal.");
                         break;
                     }
                 }
-                if (!hasFreeSlot()) {
-                    break;
-                }
             }
-        }
 
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await chest.close();
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        bot.chat('/is shop Food');
-    }, 500);
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            await chest.close();
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            bot.chat('/is shop Food');
+        }, 500);
+    }
 }
